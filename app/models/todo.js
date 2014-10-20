4ea68e923c71415b968aee8f73f2c4d8ef21824e
Lindsay Verola <lindsayv@squareup.com>
2014-10-19 23:50:08 -0400

    Display the model fixture data

--- app/models/todo.js
+++ app/models/todo.js
@@ -1,5 +1,28 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
-  
+var Todo = DS.Model.extend({
+  title: DS.attr('string'),
+  isCompleted: DS.attr('boolean')
 });
+
+Todo.reopenClass({
+  FIXTURES: [
+    {
+      id: 1,
+      title: 'Update my resume!',
+      isCompleted: true
+    },
+    {
+      id: 2,
+      title: 'Answer short answer code camp questions',
+      isCompleted: false
+    },
+    {
+      id: 3,
+      title: 'Apply to code camp!',
+      isCompleted: false
+    }
+  ]
+});
+
+export default Todo;
